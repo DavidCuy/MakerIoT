ccf46e91d2c56ea0e9e535ea2b29f467b9fcc2e6
David Cuy <david.cuy.sanchez@gmail.com>
2022-03-15 23:28:34 -0600

    feature: integracion de cabmio de nombrres en paginas

--- web-portal/src/app/app.component.ts
+++ web-portal/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iot-portal';
+
+  pageName: string = ''
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+  update_pagename(pageName: string): void {
+    this.pageName = pageName
+  }
 }
--- web-portal/src/app/components/sidebar/sidebar.component.ts
+++ web-portal/src/app/components/sidebar/sidebar.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, DoCheck {
+  @Output() pageName_emitter = new EventEmitter<string>();
+  @Output() breadcrumbs_emitter = new EventEmitter<string[]>();
 
-  menu_items: any = []
+  menu_items: Array<any> = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      url: '/dashboard'
+    },
+    {
+      title: 'MQTT Test',
+      icon: 'network_wifi',
+      url: '/mqtt-test'
+    }
+  ]
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
-    this.menu_items = [
-      {
-        title: 'Dashboard',
-        icon: 'dashboard',
-        url: '/dashboard'
-      },
-      {
-        title: 'MQTT Test',
-        icon: 'network_wifi',
-        url: '/mqtt-test'
-      }
-    ]
+  }
+
+  ngDoCheck(): void {
+    const menu_item = this.menu_items.find(mi => mi.url === this._router.url)
+    if (menu_item === undefined) return
+    this.breadcrumbs_emitter.emit(menu_item.title)
+    this.pageName_emitter.emit(menu_item.title)
   }
 
 }
