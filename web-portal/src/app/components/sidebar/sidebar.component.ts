045ca175670d5c0f727bce688c64e1dea53f6535
David Cuy <david.cuy.sanchez@gmail.com>
2022-03-15 23:00:49 -0600

    feature: integracion de menú dinámico

--- web-portal/src/app/components/sidebar/sidebar.component.ts
+++ web-portal/src/app/components/sidebar/sidebar.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  menu_items: any = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.menu_items = [
+      {
+        title: 'Dashboard',
+        icon: 'dashboard',
+        url: '/dashboard'
+      },
+      {
+        title: 'MQTT Test',
+        icon: 'network_wifi',
+        url: '/mqtt-test'
+      }
+    ]
   }
 
 }
